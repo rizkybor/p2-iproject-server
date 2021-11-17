77f71e7106a1905715aa5e866146c81108f55b83
rizkybor <rizkyak994@gmail.com>
2021-11-17 15:51:50 +0700

    update2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 
 const Controller = require('../controllers')
-const erorHandler = require('../middlewares/errorHandler')
 const Account = require('../controllers/account')
 const authentication = require('../middlewares/authentication')
 
@@ -9,8 +8,8 @@ router.post('/register', Account.register)
 router.post('/login', Account.login)
 router.use(authentication)
 router.get('/heroes', Controller.getHeros)
-router.post('/myheroes/:id')
+// router.post('/myheroes/:id')
 
-router.use(erorHandler)
+// router.use(erorHandler)
 
 module.exports = router
