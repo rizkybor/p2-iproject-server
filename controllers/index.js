9307fb43817a941610cab127ddfac55f3415de97
rizkybor <rizkyak994@gmail.com>
2021-11-17 17:17:04 +0700

    berhasil fetching

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const { Mountain, Listing, User } = require("../models")
+const axios = require('axios')
 
 class Controller {
     static async getMountain(req, res, next) {
         try{
-           console.log ('masuk controller get mountain')
+        //    console.log ('masuk controller get mountain')
+           const response = await axios.get('https://indonesia-public-static-api.vercel.app/api/volcanoes')
+           console.log (response)
+           res.status(200).json({
+               message: 'Berhasil Fetching Rest API Gunung Indonesia',
+               data: response.data
+           })
         }
         catch(err){
             res.send(err)
